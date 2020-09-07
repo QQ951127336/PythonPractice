97f1a84a0a2d023a1f62d526d3f37eb2568853fd
zhoukeyu <zhoukeyu@lizhi.fm>
2020-09-07 22:52:04 +0800

    修改冒泡排序错误

--- sort/bubblingSort.py
+++ sort/bubblingSort.py
@@ -2,14 +2,19 @@
 # -*- coding:utf-8 -*-
 
 from sort.order import Order
+from util.randomUtil import RandomUtil
+
 
 def bubblingSort(dataList, order=Order.AES):
     factor = 1
     if order == Order.DES:
         factor = -1
-    for length in range(len(dataList) - 1, 1, -1):
-        for index in range(0, length-1):
+    for length in range(len(dataList), 0, -1):
+        for index in range(0, length - 1):
             if (dataList[index] - dataList[index + 1]) * factor > 0:
                 dataList[index], dataList[index + 1] = dataList[index + 1], dataList[index]
     return dataList
 
+
+if __name__ == '__main__':
+    print(bubblingSort(RandomUtil().getRandomList(10, 0, 100), Order.AES))

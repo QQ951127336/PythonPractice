b85d29bb63085ce3c9e8673065fff9d65ffa928b
zhoukeyu <zhoukeyu@lizhi.fm>
2020-09-07 22:32:17 +0800

    新增随机生成列表

---
+++ util/randomUtil.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+import random
+
+class RandomUtil():
+
+    def getRandomList(self, length, min, max):
+        data = []
+        while(length > 0):
+            data.append(random.randint(min, max))
+            length = length - 1
+        return data
+
+
+if __name__ == '__main__':
+    randomUtil = RandomUtil()
+    print(randomUtil.getRandomList(10, 0, 10000))

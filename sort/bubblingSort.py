7f675fab3299db95fe791af5bdcd55c71b4364d8
zhoukeyu <zhoukeyu@lizhi.fm>
2020-09-07 22:25:38 +0800

    冒泡算法

--- sort/bubblingSort.py
+++ sort/bubblingSort.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
-from enum import Enum
 
-class Order(Enum):
-    AES = 0
-    DES = 1
+from sort.order import Order
 
 def bubblingSort(dataList, order=Order.AES):
+    factor = 1
+    if order == Order.DES:
+        factor = -1
+    for length in range(len(dataList) - 1, 1, -1):
+        for index in range(0, length-1):
+            if (dataList[index] - dataList[index + 1]) * factor > 0:
+                dataList[index], dataList[index + 1] = dataList[index + 1], dataList[index]
+    return dataList
+
---
+++ sort/order.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+from enum import Enum
+
+class Order(Enum):
+    AES = 0
+    DES = 1

de216e57b5c2b8bebb9127063d2821211268dc73
zhoukeyu <zhoukeyu@lizhi.fm>
2020-09-11 11:21:30 +0800

    增加二叉搜索树插入，并修改遍历Bug

--- tree/tree.py
+++ tree/tree.py
@@ -37,21 +37,54 @@ class BiTreeNode:
 
 def traversePre(node):
     if node is not None:
-        print(node.data)
-        traversePre(node.left)
-        traversePre(node.right)
+        print(node.data, end=" ")
+        traversePre(node.getLeft())
+        traversePre(node.getRight())
 
 def traverseMid(node):
     if node is not None:
-        traversePre(node.left)
-        print(node.data)
-        traversePre(node.right)
+        traverseMid(node.getLeft())
+        print(node.data, end=" ")
+        traverseMid(node.getRight())
 
 def traverseLast(node):
     if node is not None:
-        traversePre(node.left)
-        traversePre(node.right)
-        print(node.data)
+        traverseLast(node.getLeft())
+        traverseLast(node.getRight())
+        print(node.data, end=" ")
+
+def maxDeep(node):
+    if node is not None:
+        leftDeep = maxDeep(node.getLeft())
+        rightDeep = maxDeep(node.getRight())
+        return max(leftDeep, rightDeep) + 1
+    return 0
+
+def insertBST(node, value):
+    if node:
+        if node.getValue() < value:
+            if node.getRight():
+                insertBST(node.getRight(), value)
+            else:
+                node.setRight(value)
+        elif node.getValue() > value:
+            if node.getLeft():
+                insertBST(node.getLeft(), value)
+            else:
+                node.setLeft(value)
+        return node
+    return BiTreeNode(value)
 
 
+if __name__ == '__main__':
+    root = insertBST(None, 3)
+    insertBST(root, 3)
+    insertBST(root, 2)
+    insertBST(root, 4)
+    insertBST(root, 5)
+    insertBST(root, 1)
+    traversePre(root)
+    print("")
+    print("------------")
+    traverseMid(root)
 
